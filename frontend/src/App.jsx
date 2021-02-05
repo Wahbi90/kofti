070774d3327447a6200f1b660c47e1f9752323ba
ramisayeh <rami6sayeh@gmail.com>
2021-02-05 10:42:05 +0100

    unfinished cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,26 +5,29 @@ import Sidebar from "./components/sidebar/sidebar";
 import CardsList from "./components/cardsList/cardsList";
 import Navside from "./components/navSide/navSide";
 import { Layout, Menu, Breadcrumb } from 'antd';
-import  Cart from "./components/cart/cart"
-
+import  Cart from "./components/cart/cart";
+import { Provider } from "react-redux";
+import store from "./store";
 
 
 function App() {
   const { Header, Content, Footer } = Layout;
   
   return (
-    <div className="App">
+    <Provider store={store}>
+      <div className="App">
       <Navbar/> 
      <Sidebar/>
-     <div className="site-layout-background" style={{ padding: 250, marginTop: -150}}>
-     <CardsList/>  
-        {/* <Home/>      */}
+      <div className="site-layout-background" style={{ padding: 250, marginTop: -150}}>
+    {/* <CardsList/>   */}
+    `    <Home/>     `
      {/* <Cart/> */}
       </div>
    
        {/* <Navside/> */}
       
     </div>
+    </Provider>
   );
 }
 
